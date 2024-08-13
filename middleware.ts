4b65c2cd7b27b8e2f5c9cdabaf1d94ea8d3842e2
manta33 <sksmstmdals3@gmail.com>
2024-08-14 00:35:31 +0900

    #8.10 Matcher

--- middleware.ts
+++ middleware.ts
@@ -3,3 +3,7 @@ import { NextRequest } from "next/server";
 export function middleware(request: NextRequest) {
   console.log(request.nextUrl.pathname);
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
